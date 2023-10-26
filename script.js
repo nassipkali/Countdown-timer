0a1e79391a0787eec156f40b8e89bcd6b283ea28
Daulet Nassipkali <uralskdev@gmail.com>
2023-10-27 00:59:12 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
 const year = new Date().getFullYear();
 const myDate = new Date('Oct 27, 2023 16:47:00');
-console.log(myDate);
 
-// countdown
-let timer = setInterval(function() {
+if (isNaN(myDate)) {
+    console.error('Invalid date format for myDate');
+} else {
+    let timer = setInterval(function() {
+        const today = new Date().getTime();
+        const diff = myDate - today;
+        
+        if (diff <= 0) {
+            clearInterval(timer); // Stop the countdown when the target date is reached
+        } else {
+            let days = Math.floor(diff / (1000 * 60 * 60 * 24));
+            let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+            let seconds = Math.floor((diff % (1000 * 60)) / 1000);
 
-// get today's date
-const today = new Date().getTime();
-
-// get the difference
-const diff = myDate - today;
-
-// math
-let days = Math.floor(diff / (1000 * 60 * 60 * 24));
-let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-let seconds = Math.floor((diff % (1000 * 60)) / 1000);
-
-// display
-document.getElementById("days").innerHTML=days
-document.getElementById("hours").innerHTML=hours
-document.getElementById("minutes").innerHTML=minutes
-document.getElementById("seconds").innerHTML=seconds
-
-}, 
-1);
+            document.getElementById("days").innerHTML = days;
+            document.getElementById("hours").innerHTML = hours;
+            document.getElementById("minutes").innerHTML = minutes;
+            document.getElementById("seconds").innerHTML = seconds;
+        }
+    }, 1000); // 1-second interval
+}
