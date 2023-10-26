a184950e626353576f12cac260265e9aa81a3dcc
Daulet Nassipkali <uralskdev@gmail.com>
2023-10-27 01:05:54 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-const year = new Date().getFullYear();
-const myDate = new Date('Oct 27, 2023 16:47:00');
+const myDate = new Date('Oct 27, 2023 16:47:00 UTC'); // Append "UTC" to specify the time zone
 
 if (isNaN(myDate)) {
     console.error('Invalid date format for myDate');
 } else {
     let timer = setInterval(function() {
-        const today = new Date().getTime();
-        const diff = myDate - today;
+        const now = new Date(); // Get the current time in the user's local time zone
+        const nowUTC = new Date(now.toISOString()); // Convert to UTC by using ISO string
         
-        if (diff <= 0) {
+        if (myDate <= nowUTC) {
             clearInterval(timer); // Stop the countdown when the target date is reached
         } else {
+            const diff = myDate - nowUTC;
             let days = Math.floor(diff / (1000 * 60 * 60 * 24));
             let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
             let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
@@ -23,3 +23,4 @@ if (isNaN(myDate)) {
         }
     }, 1000); // 1-second interval
 }
+
